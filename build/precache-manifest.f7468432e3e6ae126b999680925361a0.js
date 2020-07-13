f4188e1e84744e1d540ea670fabb1578731de4e9
Joseph Butterfield <raleigh9123@gmail.com>
2020-07-12 21:57:29 -0700

    Update Default URL Param

--- build/precache-manifest.f7468432e3e6ae126b999680925361a0.js
+++ build/precache-manifest.f7468432e3e6ae126b999680925361a0.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "51f8b34f686185ad5d9899bc2f7bb66f",
+    "revision": "0ef22b00fc79777f2e0126910ad2707c",
     "url": "/React-Gallery-App/index.html"
   },
   {
-    "revision": "55bef97b5c3853a3e778",
+    "revision": "9b77b529fa3a4d98e147",
     "url": "/React-Gallery-App/static/css/main.58e89fa6.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Gallery-App/static/js/2.99d32a86.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "55bef97b5c3853a3e778",
-    "url": "/React-Gallery-App/static/js/main.409e3c50.chunk.js"
+    "revision": "9b77b529fa3a4d98e147",
+    "url": "/React-Gallery-App/static/js/main.ef428a87.chunk.js"
   },
   {
     "revision": "75268972c1a3373e6f79",
--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -7,17 +7,20 @@ const PhotoContainer = ({data}) => {
 
     //Take search parameters from the URL and pass string into the data object to return queried object
     let {search} = useParams();
+    if(search === 'React-Gallery-App') {
+      search = 'mountain';
+    }
     let dataObject = data[`${search}`]
-    
+
     //Declare photos variable. .map() over the object and return photo components for each image in object.
     let photos='';
     if(dataObject) {
         let photosMap = dataObject.map(image => <Photo id={image.id} farmID={image.farm} serverID={image.server} secret={image.secret} title={image.title} key={image.id}/>)
         photos = photosMap;
-    } 
+    }
     //Determine if photos exist. If not, will return value '0'
     let photosExist = photos.length;
-     
+
     //If photos exist, load header text for query, else header will produce 'loading...'
     //If photos exist, render the photos, else render Not Found component
     return (
